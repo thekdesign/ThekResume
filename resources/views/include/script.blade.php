fcf1a3c2764e3658747b13a5a23ec0b0f2617212
ThekDesign <kevin830802@gmail.com>
2018-02-03 23:38:13 +0800

    :pencil2: code

--- resources/views/include/script.blade.php
+++ resources/views/include/script.blade.php
@@ -1,6 +1,7 @@
 <!-- External JavaScripts
 ============================================= -->
 <script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/plugins.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/lazyload@2.0.0-beta.2/lazyload.js"></script>
 
 <!-- Footer Scripts
