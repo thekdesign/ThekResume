65234f62489e0eaa396bb0cdc5803583a860252d
ThekDesign <kevin830802@gmail.com>
2018-02-04 01:09:14 +0800

    :pencil2: code

--- resources/views/include/script.blade.php
+++ resources/views/include/script.blade.php
@@ -1,7 +1,23 @@
 <!-- External JavaScripts
 ============================================= -->
 <script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/plugins.js"></script>
+{{--<script type="text/javascript" src="js/plugins/jquery.bootstrap.js"></script>--}}
+<script type="text/javascript" src="js/plugins/jquery.chart.js"></script>
+{{--<script type="text/javascript" src="js/plugins/jquery.color.js"></script>--}}
+{{--<script type="text/javascript" src="js/plugins/jquery.cookie.js"></script>--}}
+{{--<script type="text/javascript" src="js/plugins/jquery.countdown.js"></script>--}}
+<script type="text/javascript" src="js/plugins/jquery.countto.js"></script>
+{{--<script type="text/javascript" src="js/plugins/jquery.dribbble.js"></script>--}}
+{{--<script type="text/javascript" src="js/plugins/jquery.fitvids.js"></script>--}}
+{{--<script type="text/javascript" src="js/plugins/jquery.flexslider.js"></script>--}}
+{{--<script type="text/javascript" src="js/plugins/jquery.flickrfeed.js"></script>--}}
+<script type="text/javascript" src="js/plugins/jquery.form.js"></script>
+<script type="text/javascript" src="js/plugins/jquery.important.js"></script>
+<script type="text/javascript" src="js/plugins/jquery.infinitescroll.js"></script>
+
+
+
+{{--<script type="text/javascript" src="js/plugins.js"></script>--}}
 <script src="https://cdn.jsdelivr.net/npm/lazyload@2.0.0-beta.2/lazyload.js"></script>
 
 <!-- Footer Scripts
